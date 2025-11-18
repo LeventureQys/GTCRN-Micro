8a72275056e362b24896d78d1f101ee056e77bba
bglid <benjamin.h.glidden@gmail.com>
2025-11-18 10:14:14 -0500

    recreating venv and adding unit test

---
+++ tests/models/test_gtcrn_micro.py
@@ -0,0 +1,37 @@
+import torch
+from gtcrn_micro.models.gtcrn_micro import GTCRNMicro
+
+
+def test_gtcrn_micro():
+    # causal test for workflow
+    model = GTCRNMicro().eval()
+
+    """causality check"""
+    a = torch.randn(1, 16000)
+    b = torch.randn(1, 16000)
+    c = torch.randn(1, 16000)
+    x1 = torch.cat([a, b], dim=1)
+    x2 = torch.cat([a, c], dim=1)
+
+    x1 = torch.stft(
+        x1, 512, 256, 512, torch.hann_window(512).pow(0.5), return_complex=False
+    )
+    x2 = torch.stft(
+        x2, 512, 256, 512, torch.hann_window(512).pow(0.5), return_complex=False
+    )
+    y1 = model(x1)[0]
+    y2 = model(x2)[0]
+
+    # apparently newer torch wants complex
+    y1 = torch.view_as_complex(y1.contiguous())
+    y2 = torch.view_as_complex(y2.contiguous())
+
+    y1 = torch.istft(y1, 512, 256, 512, torch.hann_window(512).pow(0.5))
+    y2 = torch.istft(y2, 512, 256, 512, torch.hann_window(512).pow(0.5))
+
+    # y1 = y1.squeeze(0)
+    # y2 = y2.squeeze(0)
+
+    res1 = (y1[: 16000 - 256 * 2] - y2[: 16000 - 256 * 2]).abs().max()
+    # print((y1[16000:] - y2[16000:]).abs().max())
+    assert res1.item() == 0
