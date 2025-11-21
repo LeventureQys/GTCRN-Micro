08bda848eb53a457771fcb0b1a748af27a2907b4
bglid <benjamin.h.glidden@gmail.com>
2025-11-21 17:07:27 -0500

    Switching quantization method to onnx2tf int8

--- scripts/onnx2tf.sh
+++ scripts/onnx2tf.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 
 # run from root
 ONNX_INPUT="gtcrn_micro/models/onnx/"
-# ONNX_FILE=gtcrn_micro.onnx
 ONNX_FILE=gtcrn_micro.onnx # testing lowered opset 16
 OUTPUT_PATH="gtcrn_micro/models/tflite/"
 JSON_FILE=replace_gtcrn_micro.json
@@ -16,4 +15,6 @@ uv run onnx2tf \
 	-o "${OUTPUT_PATH}" \
 	-kat mix conv_cache tra_cache inter_cache \
 	-prf ${OUTPUT_PATH}${JSON_FILE} \
-	-cotof
+	-cotof \
+	-oiqt \
+	-qt per-channel
