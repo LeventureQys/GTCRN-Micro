4517d9cbe626dcefa09bf6b4e12aecc15502d97a
bglid <benjamin.h.glidden@gmail.com>
2025-12-06 18:46:14 -0500

    removing QAT, s3, and updating README for PR

--- scripts/train.sh
+++ scripts/train.sh
@@ -21,11 +21,3 @@ echo "Running normal training, single GPU"
 # for multiple GPUs
 # uv run python -m gtcrn_micro.train -C gtcrn_micro/conf/cfg_train_DNS3.yaml -D 0,1,2,3
 # echo "Running normal training, mult. GPUs"
-
-# for QAT training
-# uv run python -m gtcrn_micro.train_qat -C gtcrn_micro/conf/cfg_train_DNS3.yaml -D 0
-# echo "Running QAT training, single GPU"
-
-# for multiple GPUs
-# uv run python -m gtcrn_micro.train_qat -C gtcrn_micro/conf/cfg_train_DNS3.yaml -D 0,1,2,3
-# echo "Running QAT training, multiple GPUs"
